74dacf17e90a2deb88a7d014aedec5f1cc2a8822
Mpatadia <mrunalhpatadia@gmail.com>
2019-02-14 19:06:54 +0530

    data.json,home.html,home.js

--- LMS/home.js
+++ LMS/home.js
@@ -1,8 +1,70 @@
-const app={
-	pages[],
-	show: new Event('show'),
-	init: function(){
-		pages = document.queryySelectorAll('.page');
-		
-	}
-}
+
+$(document).ready(function(){
+    var i =Object.keys(data[0])[0];
+    console.log(i);
+    
+    /*for(var property in data){ 
+    	var obj = data[property];
+    	var eachkey=Object.keys(obj);
+    	console.log(eachkey);
+    	//var d = data[property];
+    	//console.log(d.eachkey[0]);
+    	console.log(obj.id);
+    	console.log(obj.userName);
+    	console.log(obj.passWord);
+    	console.log(obj.admin);
+    	console.log(obj.email);
+    	console.log(obj.course);
+    	console.log(obj.batch);
+  };*/
+
+});
+
+  //	console.log(data[property].[0]);
+  	/*var x = document.createElement("TD");
+			var t = document.createTextNode(data[property].key);
+  			x.appendChild(t);
+  			document.getElementById("student_Details").appendChild(x);
+*/  	//}
+  
+
+/*  for(var i=0; i<eachkey.length;i++){
+  	var x = document.createElement("TD");
+			var t = document.createTextNode(1);
+  			x.appendChild(t);
+  			document.getElementById("student_Details").appendChild(x);
+  }
+*/
+//var row = document.createElement("TR");
+/*var x = document.createElement("TD");
+		var t = document.createTextNode(data[property].id);
+  		x.appendChild(t);
+  		document.getElementById("student_Details").appendChild(x);*/
+ 	/*for(var i = 0;i<Object.keys(data[property]).length;i++);
+    	{
+    		var x = document.createElement("TD");
+    		var t = document.createTextNode(data[property].id);	
+    	}
+  	}*/
+  	/*document.createElement("TR");*/
+  	/*for (var i=0;i<Object.keys(obj).length;i++)  {
+  			//var x = document.createElement("TD");
+  			//var t = document.createTextNode(data[property].id);
+  			console.log(data[property].id);
+    	}*/
+
+    	//console.log(eachkey.length);
+    	/*for(var i=0; i<=eachkey.length;i++){
+  			var x = document.createElement("TD");
+			var t = document.createTextNode(1);
+  			x.appendChild(t);
+  			document.getElementById("student_Details").appendChild(x);
+  			console.log("hi");			  		
+    	}*/
+    	//console.log(eachkey[i]);
+/*    	 console.log(eachkey.length);
+  for(var j=0;j<eachkey.length;j++){
+  		console.log(data[property]);
+  	 }
+  });
+*/
